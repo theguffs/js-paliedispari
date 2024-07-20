54761c2c388b032d1bd9b7b2221b8f0f94528cce
theguffs <youssefabdelmalak01@gmail.com>
2024-07-20 17:18:44 +0200

    Numero da 1 a 5

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -10,4 +10,14 @@ function pariDispari() {
         alert("Inserisci una scelta valida: 'pari' o 'dispari'.");
         scelta = prompt("Scegli 'pari' o 'dispari':").toLowerCase();
     }
+
+    // Chiedi all'utente di inserire un numero da 1 a 5
+
+    let numero = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+    
+
+    while (isNaN(numero) || numero < 1 || numero > 5) {
+        alert("Inserisci un numero valido da 1 a 5.");
+        numero = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+    }
 }
